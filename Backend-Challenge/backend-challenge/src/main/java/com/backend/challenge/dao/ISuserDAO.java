d679d4c92deb6b8f929c57320c0ce00695b8a472
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-05-16 18:49:00 +0200

    Push Backend-Challenge

---
+++ Backend-Challenge/backend-challenge/src/main/java/com/backend/challenge/dao/ISuserDAO.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.safein.backend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.safein.backend.dto.Suser;
+
+/**
+ * @author Elena, Alejandro, Francisco
+ *
+ */
+public interface ISuserDAO extends JpaRepository<Suser, Long> {
+}
---
+++ Backend-Challenge/backend-challenge/src/main/java/com/backend/challenge/service/SUserServiceImpl.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.safein.backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.safein.backend.dao.ISuserDAO;
+import com.safein.backend.dto.Suser;
+
+
+
+@Service
+public class SUserServiceImpl implements ISUserService {
+	@Autowired
+	ISuserDAO iUserDAO;
+
+	 /**Get: List all users */
+	@Override
+	public List<Suser> listUsers() {
+
+		return iUserDAO.findAll();
+	}
+
+	 /**Create: Save an user */
+	@Override
+	public Suser saveUser(Suser user) {
+
+		return iUserDAO.save(user);
+	}
+
+	/**Get: Read info about an user */
+	@Override
+	public Suser getUserById(Long id) {
+		
+		return iUserDAO.findById(id).get();
+	}
+
+	/**Update: an user */
+	@Override
+	public Suser updateUser(Suser user) {
+
+		return iUserDAO.save(user);
+	}
+
+	/** Delete: an user*/
+	@Override
+	public void deleteUser(Long id) {
+		iUserDAO.deleteById(id);
+	}
+}
